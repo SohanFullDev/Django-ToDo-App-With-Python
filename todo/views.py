9a970d31a0e2371dde51dd10eefeb4b76390bf4a
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-11 14:30:36 +0600

    add task function-cot37_B

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Task
 
 # Create your views here.
@@ -7,4 +7,5 @@ def addTask(request):
     #print(request.POST['task'])
     task = request.POST['task']
     Task.objects.create(task=task)
-    return HttpResponse('The form is submitted')
+    #return HttpResponse('The form is submitted')
+    return redirect('home')
