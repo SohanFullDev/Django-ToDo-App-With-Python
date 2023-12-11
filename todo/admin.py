fc4f35ca4d8557cdc01a9c5b8c91a71f8cc4b830
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-11 13:41:45 +0600

    admin list display-cot35_B

--- todo/admin.py
+++ todo/admin.py
@@ -4,5 +4,5 @@ from .models import Task
 # Register your models here.
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('task', 'is_completed', 'updated_at')
-
+    search_fields = ('task', )
 admin.site.register(Task, TaskAdmin)
