b18430e551e6ce1a8054d5607cc842411ffab570
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-12 16:09:29 +0600

    completed tasks-cot38_B

--- todo_main/views.py
+++ todo_main/views.py
@@ -7,8 +7,9 @@ def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
     
     completed_tasks = Task.objects.filter(is_completed=True)
-    print(completed_tasks)
+    #print(completed_tasks)
     context = {
         'tasks': tasks,
+        'completed_tasks': completed_tasks,
     }
     return render(request, 'home.html', context)
