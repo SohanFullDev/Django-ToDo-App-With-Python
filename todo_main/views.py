e774680dfe8847a2c02c2bb9aec84f4873f006ac
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-12 15:02:13 +0600

    completed tasks-cot38-A

--- todo_main/views.py
+++ todo_main/views.py
@@ -5,6 +5,9 @@ from todo.models import Task
 def home(request):
    # tasks = Task.objects.filter(is_completed=False).order_by('updated_at')
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    
+    completed_tasks = Task.objects.filter(is_completed=True)
+    print(completed_tasks)
     context = {
         'tasks': tasks,
     }
