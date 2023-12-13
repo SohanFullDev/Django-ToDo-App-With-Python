0dad96379137e8690dedf42a4fef27714ca9c308
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-13 13:43:52 +0600

    delete task-cot43

--- todo/urls.py
+++ todo/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
     #edit feature
     path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+    #delete task
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
 
 ]
